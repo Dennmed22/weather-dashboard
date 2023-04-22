ff3ed5477b815c453d223d290e4ce1399ea8ea7b
Dennmed22 <medrano.dennis@yahoo.com>
2023-04-22 16:45:44 -0400

    added final README

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const city = document.querySelector('#cityoutput')
 const descrip = document.querySelector('#description')
 const temp = document.querySelector('#temp')
 const wind = document.querySelector('#wind')
+const maxTemp = document.querySelector('#tempMax')
 
 
-
-function getApi() {
+function getApi(city) {
 const weatherLink = 'https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid=73c15a19bebffba4ddf655737bbc2b28&units=imperial'
 fetch(weatherLink)
 .then(function (response) {
@@ -23,3 +23,19 @@ fetch(weatherLink)
 });
 }
 fetchButton.addEventListener('click', getApi);
+
+function getFive(city) {
+   const fiveDay = //'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=e79c26882ea37d82f93d1a156667b3d6'
+   'https://pro.openweathermap.org/data/2.5/weather?'+inputval.value+'&appid=e79c26882ea37d82f93d1a156667b3d6'
+   //'https://pro.openweathermap.org/data/2.5/forecast/hourly?'+inputval.value+'&appid=e79c26882ea37d82f93d1a156667b3d6&units=imperial'
+  fetch(fiveDay)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    maxTemp.innerText = data.main.temp_max;
+  });
+  }
+  fetchButton.addEventListener('click', getFive);
+
